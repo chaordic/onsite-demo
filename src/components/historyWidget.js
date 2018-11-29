33bef7bb658d3abe98954ec8b81b4492f1fbb643
Eduardo Lopes <eduardo.lopes@neemu.com>
2018-11-29 16:52:55 -0400

    feat: add highlight to reference selected

--- src/components/historyWidget.js
+++ src/components/historyWidget.js
@@ -10,11 +10,27 @@ import templateHistoryWidget from '../../layout/templates/historyWidget.ejs';
 // import templateLoading from '../../layout/templates/components/loading.ejs';
 
 export const HistoryWidget = {
+  listenRefresh(widget) {
+    const highlight = 'border border-primary';
+    const highlightClass = '.references-card';
+    const referencesCards = $(`#${widget.id}`).find(highlightClass);
+
+    referencesCards.mousedown(function () {
+      referencesCards.parent()
+        .parent()
+        .siblings()
+        .find(highlightClass)
+        .removeClass(highlight);
+      $(this).addClass(highlight);
+    });
+  },
+
   getHtml(widget) {
     return ejs.render(templateHistoryWidget, { widget });
   },
 
   render(widget, field) {
     $(`.${field}`).append(this.getHtml(widget));
+    this.listenRefresh(widget);
   },
 };
